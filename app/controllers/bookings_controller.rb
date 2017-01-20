7c0bb00da45edea6b7e4e411f58f817a49b42a0c
Robert Jan Hogerbrugge <rj.hogerbrugge@hotmail.com>
2017-01-20 11:49:23 +0100

    Through Asssociation: Views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,18 @@
 class BookingsController < ApplicationController
   before_action :authenticate_user!
 
-    def create
-      @booking = current_user.bookings.create(booking_params)
+  def create
+    @booking = current_user.bookings.new(booking_params)
+    @booking.room_id = params[:room_id]
+    @booking.set_total_price
+    @booking.save
 
-      redirect_to @booking.room, notice: "Thank you for booking!"
-    end
+    redirect_to @booking.room, notice: "Thank you for booking!"
+  end
 
-    private
+  private
 
     def booking_params
-      params.require(:booking).permit(:starts_at, :ends_at, :price, :total, :room_id)
+      params.require(:booking).permit(:starts_at, :ends_at)
     end
 end
